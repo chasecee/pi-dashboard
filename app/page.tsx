87136f20cc709694a2a1f06478c24066fedce5eb
Chase Christensen <chaserchristensen@gmail.com>
2024-01-07 15:18:50 -0700

    cool

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       {/* <Clock /> */}
-      Test
+      Testy
     </main>
   );
 }
