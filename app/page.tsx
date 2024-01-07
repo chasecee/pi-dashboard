148fe9c11f2952bd728a4cdd2f8ea82f5082a8fe
Chase Christensen <chaserchristensen@gmail.com>
2024-01-07 15:09:47 -0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import Clock from "./components/Clock";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <Clock />
+      {/* <Clock /> */}
+      Test
     </main>
   );
 }
