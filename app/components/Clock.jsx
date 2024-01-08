f798b479060fca0b43b2088db11d3e6d076423e2
Chase Christensen <chaserchristensen@gmail.com>
2024-01-08 11:28:10 -0700

    buildd

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -27,7 +27,7 @@ export default function Clock() {
   }, []);
 
   return (
-    <div className="w-full text-center text-[14vw]">
+    <div className="w-full text-center text-[14vw] text-red-900">
       {time || "00:00:00 PM"}
     </div>
   );
