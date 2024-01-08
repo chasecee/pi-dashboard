ce8785ea67953da37aad8340de4a067f0ca77763
Chase Christensen <chaserchristensen@gmail.com>
2024-01-07 17:13:58 -0700

    error build

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Clock from "./components/Clock";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="rounded-xl ring-[10px] ring-white/50 px-10 transform-gpu rotate-90">
+      <div className="rounded-xl ring-[8px] ring-white/50 px-10 transform-gpu rotate-90">
         <Clock />
       </div>
     </main>
