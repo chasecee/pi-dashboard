36e926e7cac0b0b83f0c915f8463b976503350c6
Chase Christensen <chaserchristensen@gmail.com>
2024-01-07 20:49:45 -0700

    trigger build

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -2,21 +2,33 @@
 import { useState, useEffect } from "react";
 
 export default function Clock() {
-  const [time, setTime] = useState(new Date());
+  const [time, setTime] = useState(null);
+
+  const formatTime = (date) => {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    const ampm = hours >= 12 ? "PM" : "AM";
+
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    return `${hours}:${minutes}:${seconds} ${ampm}`;
+  };
 
   useEffect(() => {
-    const timerId = setInterval(() => setTime(new Date()), 1000);
+    const timerId = setInterval(() => {
+      setTime(formatTime(new Date()));
+    }, 1000);
     return () => clearInterval(timerId);
   }, []);
 
   return (
     <div className="w-full text-center text-[12vw]">
-      {time.toLocaleTimeString("en-US", {
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        hour12: true,
-      })}
+      {time || "00:00:00 AM"}
     </div>
   );
 }
