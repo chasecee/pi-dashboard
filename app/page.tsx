ad61f8fa5f416ab1635d3595d2126decf297632d
Chase Christensen <chaserchristensen@gmail.com>
2024-01-08 10:54:08 -0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Clock from "./components/Clock";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="rounded-xl ring-[8px] ring-red/50 px-20 ">
+      <div className="rounded-xl ring-[8px] ring-red/50 px-10 ">
         <Clock />
       </div>
     </main>
