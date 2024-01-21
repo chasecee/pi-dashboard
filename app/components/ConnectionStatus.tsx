a0c17396bffae66b25b56bd03f284b18c3bc9967
Chase Christensen <chaserchristensen@gmail.com>
2024-01-21 14:34:47 -0700

    fix

--- app/components/ConnectionStatus.tsx
+++ app/components/ConnectionStatus.tsx
@@ -2,13 +2,17 @@
 import React, { useState, useEffect } from "react";
 
 const ConnectionStatus = () => {
-  const [isOnline, setIsOnline] = useState(navigator.onLine);
-
-  const updateStatus = () => {
-    setIsOnline(navigator.onLine);
-  };
+  const [isOnline, setIsOnline] = useState(false); // Initially false
+  const [hasMounted, setHasMounted] = useState(false); // Track if component has mounted
 
   useEffect(() => {
+    setHasMounted(true); // Set to true on client-side mount
+    setIsOnline(navigator.onLine); // Set actual online status after mounting
+
+    const updateStatus = () => {
+      setIsOnline(navigator.onLine);
+    };
+
     window.addEventListener("online", updateStatus);
     window.addEventListener("offline", updateStatus);
 
@@ -18,9 +22,13 @@ const ConnectionStatus = () => {
     };
   }, []);
 
+  if (!hasMounted) {
+    return null; // Render nothing on the server
+  }
+
   return (
     <div
-      className={`flex rounded-l-xl ring-[6px] flex-row items-center text-[2vw] gap-4 px-4 py-2 ring-current ring-opacity-50  uppercase font-normal flex-shrink-0 ${
+      className={`flex rounded-l-xl ring-[6px] flex-row items-center text-[2vw] gap-4 px-4 py-2 ring-current ring-opacity-50 uppercase font-normal flex-shrink-0 ${
         isOnline ? "text-green-700" : "text-red-700"
       }`}
     >
