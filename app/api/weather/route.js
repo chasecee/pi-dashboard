3f0100fc71011cb0574b67ce6ee18a5ef0392a27
Chase Christensen <chaserchristensen@gmail.com>
2024-01-21 11:29:42 -0700

    safe openweather

---
+++ app/api/weather/route.js
@@ -0,0 +1,35 @@
+export async function GET() {
+  try {
+    const lat = process.env.LAT;
+    const lon = process.env.LON;
+    const apiKey = process.env.OPENWEATHER_API_KEY;
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily,minutely&units=imperial&appid=${apiKey}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+    //console.log(data);
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    return new Response(
+      JSON.stringify({ message: "Error fetching weather data" }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+}
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -6,19 +6,24 @@ const Weather = () => {
   const [weatherData, setWeatherData] = useState(null);
 
   useEffect(() => {
-    const fetchWeatherData = () => {
-      fetch(
-        `https://api.openweathermap.org/data/2.5/onecall?lat=40.738051&lon=-111.823610&exclude=hourly,daily&units=imperial&appid=bf1091302b17461664744d38b5328516`
-      )
-        .then((response) => response.json())
-        .then((data) => setWeatherData(data));
+    const fetchWeatherData = async () => {
+      try {
+        const response = await fetch("/api/weather");
+        if (!response.ok) {
+          throw new Error("Weather data fetch failed");
+        }
+        const data = await response.json();
+        setWeatherData(data);
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+      }
     };
 
     // Call it once immediately
     fetchWeatherData();
 
     // Then set it to be called every minute
-    const intervalId = setInterval(fetchWeatherData, 60000); // 60000 ms = 1 minute
+    const intervalId = setInterval(fetchWeatherData, 120000);
 
     // Clear interval on component unmount
     return () => clearInterval(intervalId);
@@ -26,14 +31,14 @@ const Weather = () => {
 
   if (!weatherData) return <div>Loading...</div>;
   return (
-    <div className="flex flex-row justify-between gap-0  divide-green-900 divide-x-[6px] ">
+    <div className="flex flex-row justify-between gap-0 divide-green-900 divide-x-[6px]">
       <div className="pr-2 py-4 pl-10 text-center whitespace-nowrap min-w-[40%]">
         <span className="opacity-0 hidden">°</span>
-        {weatherData.current.temp.toFixed(1)}°
+        {weatherData.current?.temp.toFixed(1)}°
       </div>
       <div className="tracking-normal flex flex-row flex-grow items-center justify-center gap-6 px-6 overflow-hidden whitespace-nowrap">
         <div className="icon flex-shrink-0 opacity-70">
-          {weatherData.current.weather && weatherData.current.weather[0] && (
+          {weatherData.current?.weather && weatherData.current.weather[0] && (
             <Image
               src={`https://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`}
               alt="Weather icon"
@@ -42,9 +47,8 @@ const Weather = () => {
             />
           )}
         </div>
-
         <p className="text-[4vw]">
-          {weatherData.current.weather && weatherData.current.weather[0]
+          {weatherData.current?.weather && weatherData.current.weather[0]
             ? weatherData.current.weather[0].description
             : "No weather data available"}
         </p>
