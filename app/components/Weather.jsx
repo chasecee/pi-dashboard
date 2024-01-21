2ed36a08a76a73e0d609978bd0228fcec7882aaf
Chase Christensen <chaserchristensen@gmail.com>
2024-01-21 12:32:09 -0700

    go

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -32,20 +32,23 @@ const Weather = () => {
   if (!weatherData) return <div>Loading...</div>;
   return (
     <div className="flex flex-row justify-between gap-0 divide-green-900 divide-x-[6px]">
-      <div className="pr-2 py-4 pl-10 text-center whitespace-nowrap min-w-[40%]">
+      <div className="pr-2 py-4 pl-10 text-center whitespace-nowrap min-w-[40% tracking-wider]">
         <span className="opacity-0 hidden">°</span>
         {weatherData.current?.temp.toFixed(1)}°
       </div>
       <div className="tracking-normal flex flex-row flex-grow items-center justify-center gap-6 px-6 overflow-hidden whitespace-nowrap">
-        <div className="icon flex-shrink-0 opacity-70">
+        <div className="icon flex-shrink-0 opacity-70 relative rounded-[20px] overflow-hidden">
+          <div className="absolute inset-0 bg-green-900 mix-blend-hue z-20"></div>
           {weatherData.current?.weather && weatherData.current.weather[0] && (
             <Image
               src={`https://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`}
               alt="Weather icon"
               width={100}
               height={100}
+              className="relative z-10"
             />
           )}
+          <div className="absolute inset-0 z-0  bg-black mix-blend-multiply"></div>
         </div>
         <p className="text-[4vw]">
           {weatherData.current?.weather && weatherData.current.weather[0]
