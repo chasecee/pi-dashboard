8735bfc58c93a4247828eb33e3863252adc89606
Chase Christensen <chaserchristensen@gmail.com>
2024-08-27 16:03:51 -0600

    📝 (Clock.jsx): Improve code readability by simplifying the logic for formatting time and removing unnecessary code related to seconds and AM/PM   📝 (page.tsx): Rearrange the layout and styling to display Clock component separately and with the correct styling; also add the missing Clock component back to the page

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -7,16 +7,12 @@ export default function Clock() {
   const formatTime = (date) => {
     let hours = date.getHours();
     let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    const ampm = hours >= 12 ? "PM" : "AM";
 
     hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    hours = hours < 10 ? "0" + hours : hours;
+    hours = hours || 12; // the hour '0' should be '12'
     minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
 
-    return `${hours}:${minutes} ${ampm}`;
+    return `${hours}:${minutes}`;
   };
 
   useEffect(() => {
@@ -27,8 +23,8 @@ export default function Clock() {
   }, []);
 
   return (
-    <div className="w-full text-center text-[21vw] leading-[90%] tracking-tight">
-      {time || "00:00 PM"}
+    <div className="w-full text-center text-[35vw] leading-[90%] tracking-tight">
+      {time || "00:00"}
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,18 @@ import Quotable from "./components/Quotable";
 export default function Home() {
   return (
     <main className="flex flex-nowrap gap-[18px] min-h-screen max-h-screen overflow-hidden flex-col items-stretch justify-center p-[6px] main text-center ">
-      <div className="rounded-xl ring-[6px] text-red-700 ring-current ring-opacity-50 px-5 py-0  flex justify-center items-center flex-col flex-grow-[2] ">
-        <Clock />
-      </div>
       <div className="rounded-xl ring-[6px] text-orange-700 ring-current ring-opacity-50 px-5 py-0 text-[7vw] uppercase font-normal tracking-[1rem] flex-grow-[1] items-center justify-center flex">
         <Date />
+      </div>
+      <div className="rounded-xl ring-[6px] text-red-700 ring-current ring-opacity-50 px-5 py-0  flex justify-center items-center flex-col flex-grow-[2] ">
+        <Clock />
       </div>
       <div className="rounded-xl ring-[6px] text-amber-600 ring-current ring-opacity-50 text-[7vw] uppercase font-normal tracking-[1rem] flex-shrink-0">
         {/* <Weather /> */}
       </div>
       <div className="flex flex-row gap-[18px]">
         <ConnectionStatus />
-        <div className="rounded-xl px-4 py-2  ring-[6px] text-purple-500 ring-current ring-opacity-50 text-[2vw] uppercase font-normal flex-grow flex justify-center">
+        <div className="rounded-xl px-4 py-2  ring-[6px] text-purple-500 ring-current ring-opacity-50 text-[4vw] uppercase font-normal flex-grow flex justify-center">
           {/* <FactComponent /> */}
           <Quotable />
         </div>
